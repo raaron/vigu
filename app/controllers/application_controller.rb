b497b420435d00b7d511053cf39213bf57f71bc9
Aaron Richiger <a.richi@bluewin.ch>
2012-09-09 02:23:28 +0200

    Added support for automatic testing and for internationalization without database backend.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_locale
+
+  # def set_locale
+  #   I18n.locale = params[:locale] || I18n.default_locale
+  # end
+
+  def set_locale
+    logger.debug "* Accept-Language: #{ENV['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
+  private
+  def extract_locale_from_accept_language_header
+    if params[:locale]
+      I18n.locale = params[:locale]
+    elsif ENV['HTTP_ACCEPT_LANGUAGE']
+      ENV['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    else
+      I18n.default_locale
+    end
+  end
+
 end
--- spec/requests/developments_spec.rb
+++ spec/requests/developments_spec.rb
@@ -2,9 +2,34 @@ require 'spec_helper'
 
 describe "Development" do
   describe "Home page" do
+
     it "should have the content 'Vision Guatemala'" do
       visit '/development/home'
       page.should have_content('Vision Guatemala')
     end
+
+    it "should display german text as default" do
+      visit '/development/home'
+      page.should have_content('Deutsch')
+    end
+
+    it "should display german text for german users" do
+      ENV['HTTP_ACCEPT_LANGUAGE'] = 'de'
+      visit '/development/home'
+      page.should have_content('Deutsch')
+    end
+
+    it "should display spanish text for spanish users" do
+      ENV['HTTP_ACCEPT_LANGUAGE'] = 'es-gt'
+      visit '/development/home'
+      page.should have_content('Espanol')
+    end
+
+    it "should display english text for english users" do
+      ENV['HTTP_ACCEPT_LANGUAGE'] = 'en'
+      visit '/development/home'
+      page.should have_content('English')
+    end
+
   end
 end
